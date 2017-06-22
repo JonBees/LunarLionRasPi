4b7c46d41385c3132d7153980c54b711dcd6fd8f
sammyshitforbrains <sam.binkin@gmail.com>
2017-06-22 17:18:38 -0400

    fixed folder structure

---
+++ si/fsw/platform_inc/si_msgids.h
@@ -0,0 +1,32 @@
+/************************************************************************
+** File:
+**   $Id: SI_msgids.h  $
+**
+** Purpose: 
+**  Define Sensor In App Message IDs
+**
+** Notes:
+**
+**
+*************************************************************************/
+#ifndef _SI_msgids_h_
+#define _SI_msgids_h_
+
+
+/***************************
+** SI Command Message IDs
+***************************/
+#define SI_CMD_MID            	0x1704
+#define SI_SEND_HK_MID        	0x1705
+
+/*****************************
+** SI Telemetry Message IDs
+******************************/
+
+#define SI_HK_TLM_MID		0x0705
+
+#endif /* _SI_msgids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
